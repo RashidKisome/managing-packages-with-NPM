b06d34b8a5635c6d5f508230fb319fba0c64b7e5
Rashid <rashidkisome@gmail.com>
2020-12-21 14:36:05 +0100

    route

--- server.js
+++ server.js
@@ -58,6 +58,11 @@ if (!process.env.DISABLE_XORIGIN) {
 //       .send(err.message || "SERVER ERROR");
 //   }
 // });
+
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
+
 var port = process.env.PORT || 3000;
 bGround
   .setupBackgroundApp(app, myApp, __dirname)
