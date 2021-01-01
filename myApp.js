5c71273dd02222f3206d262b3deb7f0abcdda103
Rashid <rashidkisome@gmail.com>
2021-01-01 12:43:18 +0100

    middleware

--- myApp.js
+++ myApp.js
@@ -3,4 +3,9 @@ var app = express();
 
 console.log("Hello World");
 
+app.use(function (req, res, next) {
+  console.log(req.method + " " + req.path, +" " + req.ip);
+  next();
+});
+
 module.exports = app;
--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+// app.use(function (req, res, next) {
+//   console.log(req.method + " " + req.path, +" " + req.ip);
+//   next();
+// });
 // serve static assets
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
